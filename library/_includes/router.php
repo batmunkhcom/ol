cb1dfbfe181e2fc54caf12f0b261e15631ff74e9
BATMUNKH Moltov <contact@batmunkh.com>
2014-01-09 02:41:59 +0800

    .

--- library/_includes/router.php
+++ library/_includes/router.php
@@ -11,12 +11,12 @@
 $router->respond('GET', '/', function ($request, $response) {
 
     return 'Backend home';
-    });
+});
 
-$functions_php = \File::getFiles(DIR_CONFIG.'_includes'.DS, 'php');                                                                                                            
-foreach($functions_php as $k=>$v){                                                                                                                                          
-                                      require_once($v);                                                                                                                                                   
-                              } 
+$functions_php = \File::getFiles(DIR_CONFIG . '_includes' . DS, 'php');
+foreach ($functions_php as $k => $v) {
+    require_once($v);
+}
 
 // Run it!
 $router->dispatch();
--- web/index.php
+++ web/index.php
@@ -3,12 +3,13 @@
 define('APPMODE', 'dev');
 
 require_once '../library/bootstrap.php';
-require_once DIR_LIB.'_includes'.DS.'router.php';
+require_once DIR_LIB . '_includes' . DS . 'router.php';
 
-if(file_exists(\Config::get('load_file'))){
-    require_once \Config::get('load_file').'.php';
-}else{
+echo \Config::get('load_file');
+if (file_exists(\Config::get('load_file'))) {
+    require_once \Config::get('load_file') . '.php';
+} else {
     echo __('No such command found');
 }
-require_once DIR_LIB.'_includes/footer.php';
+require_once DIR_LIB . '_includes/footer.php';
 
