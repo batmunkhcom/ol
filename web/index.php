3aa44f8daceaba09f51c40d6752cff1df1c4a3f5
BATMUNKH Moltov <batmunkhcom@gmail.com>
2014-01-08 00:44:56 -0500

    cleanup.. update..

--- web/index.php
+++ web/index.php
@@ -7,6 +7,8 @@ require_once DIR_CONFIG.'router.php';
 
 if(file_exists(\Config::get('load_file'))){
     require_once \Config::get('load_file').'.php';
+}else{
+    echo __('No such command found');
 }
 require_once DIR_LIB.'_includes/footer.php';
 
